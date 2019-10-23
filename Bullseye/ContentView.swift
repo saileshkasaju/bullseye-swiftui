2bfcb4dd68db994aabb27e2948559960d5218cc3
Sailesh Kasaju <leosailesh@gmail.com>
2019-10-23 09:16:55 +0545

    view components laid out

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+
+  @State var alertIsVisible: Bool = false
+  
+  @State var sliderValue: Double = 50.0
+  
+  var body: some View {
+    VStack {
+
+      Spacer()
+      
+      HStack {
+        Text("Put the bullseye as close as you can to:")
+          .fontWeight(.semibold)
+          .foregroundColor(Color.green)
+        Text("100")
+      }
+
+      Spacer()
+      
+      HStack {
+        Text(/*@START_MENU_TOKEN@*/"1"/*@END_MENU_TOKEN@*/)
+        Slider(value: self.$sliderValue, in: 1...100)
+        Text(/*@START_MENU_TOKEN@*/"100"/*@END_MENU_TOKEN@*/)
+      }
+      
+      Spacer()
+      
+      Button(action: {
+        print("Button pressed!")
+        self.alertIsVisible = true
+      }){
+        Text("Hit Me!")
+      }
+      .alert(isPresented: $alertIsVisible) { () -> Alert in
+        return Alert(title: Text("Hello there"), message: Text("This is my first alert"), dismissButton: .default(Text("Awesome!")))
+      }
+      
+      Spacer()
+      
+      HStack {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+          Text("♲ Start over")
+        }
+        Spacer()
+        Text("Score")
+        Text("99999")
+        Spacer()
+        Text("Round")
+        Text("99999")
+        Spacer()
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+          Text("⚠︎ Info")
+        }
+      }
+      .padding(.bottom, 20)
+      
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView().previewLayout(.fixed(width: 896, height: 414))
+  }
 }
